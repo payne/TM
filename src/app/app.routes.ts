a629f43cc49a87688f5aaee077841eeeef2b312e
Matt Payne <Payne@MattPayne.org>
2024-11-09 19:32:44 -0600

    route not found and it goes home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'speakers', loadComponent: () => import('./pages/speakers/speakers.component').then(m => m.SpeakersComponent) },
-  { path: 'dates', loadComponent: () => import('./pages/meeting-dates/meeting-dates.component').then(m => m.MeetingDatesComponent) }
+  { path: 'dates', loadComponent: () => import('./pages/meeting-dates/meeting-dates.component').then(m => m.MeetingDatesComponent) },
+  { path: '**', redirectTo: 'home' }
 ];
