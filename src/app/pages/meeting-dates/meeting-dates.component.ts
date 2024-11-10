fea3d9e87c5a1e6ec34c1c37378486b3ef50bb77
Matt Payne <Payne@MattPayne.org>
2024-11-09 18:58:39 -0600

    ng generate component pages/MeetingDates

---
+++ src/app/pages/meeting-dates/meeting-dates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-dates',
+  standalone: true,
+  imports: [],
+  templateUrl: './meeting-dates.component.html',
+  styleUrl: './meeting-dates.component.scss'
+})
+export class MeetingDatesComponent {
+
+}
